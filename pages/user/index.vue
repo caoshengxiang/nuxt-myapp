94f3cc0e7932822ac653e2160257dc1188db6003
caoshengxiang <1104729815@qq.com>
2018-11-21 16:03:36 +0800

    'init'

---
+++ pages/user/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        $END$
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+
+</style>
